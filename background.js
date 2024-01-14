88370f7dd8b8fdba8bcccda8fe4f77f5dbce1a9d
David Öhman <dohman_92@hotmail.com>
2024-01-14 17:48:37 +0100

    Fetch url from storage in callback function

--- background.js
+++ background.js
@@ -1,11 +1,28 @@
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-    if (tab.url.includes("") && changeInfo.audible) {
-      chrome.notifications.create(
-        {
-            title: "Sound detected!",
-            message: "",
-            type: "basic"
-        }
-      );
+function restoreOptions() {
+  return new Promise((resolve, reject) => {
+    chrome.storage.sync.get({ urlText: "" }, (items) => {
+      if (chrome.runtime.lastError) {
+        reject(new Error(chrome.runtime.lastError));
+      } else {
+        resolve(items.urlText);
+      }
+    });
+  });
+}
+
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+  restoreOptions().then((url) => {
+    console.log('url', url);
+
+    if (tab.url.includes(url) && changeInfo.audible) {
+      chrome.notifications.create({
+        title: "Sound detected!",
+        message: "Message",
+        type: "basic",
+        iconUrl: "icon.png",
+      });
     }
+  }).catch((error) => {
+    console.error('Error retrieving the URL:', error);
+  });
 });
